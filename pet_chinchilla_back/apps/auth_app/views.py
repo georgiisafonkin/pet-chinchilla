618f9ab2756ebd5adfeb98bd5d40aeffb404ec31
Georgy <georgiisafonk@gmail.com>
2026-05-13 14:14:02 +0700

    auth_app ready

--- pet_chinchilla_back/apps/auth_app/views.py
+++ pet_chinchilla_back/apps/auth_app/views.py
@@ -2,11 +2,11 @@ from rest_framework import generics, permissions
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 from .models import Breeder
-from .serializers import RegisteerSerializer, CustomTokenObtainPairSerializer
+from .serializers import RegisterSerializer, CustomTokenObtainPairSerializer
 
 class RegisterView(generics.CreateAPIView):
     queryset = Breeder.objects.all()
-    serializer_class = RegisteerSerializer
+    serializer_class = RegisterSerializer
     permission_classes = (permissions.AllowAny,)
 
 class CustomTokenObtainPairView(TokenObtainPairView):

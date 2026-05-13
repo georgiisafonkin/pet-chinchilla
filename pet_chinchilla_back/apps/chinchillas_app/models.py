7668f8466281aad08932f2ee958a017ebbcde9dc
Georgy <georgiisafonk@gmail.com>
2026-05-14 01:50:59 +0700

    wip

--- pet_chinchilla_back/apps/chinchillas_app/models.py
+++ pet_chinchilla_back/apps/chinchillas_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Chinchilla(models.Model):
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='chinchillas',
+    )
+    name = models.CharField(max_length=100)
+    age = models.PositiveSmallIntegerField(null=True, blank=True)
+    breed = models.CharField(max_length=100, blank=True)
+    fur_type = models.CharField(max_length=100, blank=True)
+    color = models.CharField(max_length=100, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.name} ({self.owner.username})'
